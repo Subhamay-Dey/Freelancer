a7874325d10e8f0210dfed0c5542a69f03bca9fe
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-16 06:05:12 +0530

    Finally Supabase setup is done (both for client and server side) and also checked the new user is logged in or not though middleware and made the routes protected and also checked shadcn is brillantly working ;)

---
+++ freelancer/src/middleware/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/supabase/supabaseServer";
+import { cookies } from "next/headers";
+
+export async function middleware(request:NextRequest) {
+    const supabase = createClient(cookies())
+    const {data, error} = await supabase.auth.getUser()
+
+    //if the user is not logged in
+    if(data.user === null) {
+        return NextResponse.redirect(new URL("/login?error=Please login first to access this route.", request.url))
+    }
+
+    return NextResponse.next()
+}
+
+//protecting routes
+export const config = {
+    matcher : []
+}
