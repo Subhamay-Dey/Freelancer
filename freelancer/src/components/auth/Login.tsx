af12d54658dce8257c051c4b889e9074216d799a
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-30 02:41:52 +0530

    successfully extracted the form inputs by the useFormState hook of the react-dom and also gave the error of their validations and the user can logged in now.

--- freelancer/src/components/auth/Login.tsx
+++ freelancer/src/components/auth/Login.tsx
@@ -1,10 +1,19 @@
+"use client"
 import React from 'react';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card";
   import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
+import Loader from './Loader';
+import { useFormState } from 'react-dom';
+import { loginAction } from '@/actions/authActions';
+
+const initState = {
+  status: 0,
+  errors: {},
+}
 
 export default function Login() {
+  const [state, formAction] = useFormState(loginAction, initState)
   return (
     <Card>
           <CardHeader>
@@ -14,17 +23,24 @@ export default function Login() {
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder='Enter your email' name='email'/>
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" placeholder='Enter your password' name='email'/>
-            </div>
+            <form action={formAction}>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder='Enter your email' name='email'/>
+                <span className='text-red-500 '>{state?.errors?.email}</span>
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" placeholder='Enter your password' name='email'/>
+                <span className='text-red-500 '>{state?.errors?.password}</span>
+              </div>
+              <div>
+              <Loader/>
+              </div>
+            </form>
+
           </CardContent>
           <CardFooter>
-            <Button className='w-full'>Submit</Button>
           </CardFooter>
         </Card>
   )
--- freelancer/src/components/auth/Register.tsx
+++ freelancer/src/components/auth/Register.tsx
@@ -1,14 +1,7 @@
 "use client"
 import React from 'react';
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { registerAction } from '@/actions/authActions';
