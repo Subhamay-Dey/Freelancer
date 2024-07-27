e1dbf1f961fd2f681f951eed1fbf0bef43e579ad
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-27 18:38:47 +0530

    Added a alert dialog box for the delete function to implement only if the continue button is clicked, I did this update as the delete sign may be mistakenly clicked so for surety I gave that.

--- freelancer/src/components/common/ImagePreview.tsx
+++ freelancer/src/components/common/ImagePreview.tsx
@@ -4,12 +4,43 @@ import Image from 'next/image'
 import React from 'react'
 import { Button } from '../ui/button'
 
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+  
+
 export default function ImagePreview({image, callback}:{image:string, callback:() => void} ) {
   return (
     <div className='relative border-2 rounded-xl'>
-        <Button variant={'secondary'} size={"icon"} className='absolute top-2 right-2' onClick={callback}>
+        {/* <Button variant={'secondary'} size={"icon"} className='absolute top-2 right-2' onClick={callback}>
             <X/>
-        </Button>
+        </Button> */}
+        <AlertDialog>
+        <AlertDialogTrigger className='absolute top-2 right-2 text-white border rounded-lg bg-slate-300' >
+            <X/>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+            <AlertDialogTitle>Ready to Delete?</AlertDialogTitle>
+            <AlertDialogDescription>
+            Do you really want to delete the image?
+            </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={callback}>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+        </AlertDialog>
+
         <Image src={image} width={10} height={10} className='w-full object-contain' alt='image'/>
     </div>
   )
