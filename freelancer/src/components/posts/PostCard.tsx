437780f0e2b19007f7f342c0ad10574577f53b22
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-09 23:48:51 +0530

    postCard updated.

--- freelancer/src/components/posts/PostCard.tsx
+++ freelancer/src/components/posts/PostCard.tsx
@@ -19,17 +19,22 @@ function PostCard() {
         <MoreVertical/>
       </div>
 
-      <Image src="https://kwrzjcvsmhefihoxtrjy.supabase.co/storage/v1/object/public/freelancers/d31d9543-1754-42a5-8819-5d568f02955f/2c4cdd5d-190c-4d9e-93e4-52a84552c2e0" width={10} height={10} alt='post_image' className='w-full object-contain rounded-lg' unoptimized/>
+      <Image src="https://kwrzjcvsmhefihoxtrjy.supabase.co/storage/v1/object/public/freelancers/d31d9543-1754-42a5-8819-5d568f02955f/2c4cdd5d-190c-4d9e-93e4-52a84552c2e0" width={10} height={10} alt='post_image' className='w-full object-contain rounded-lg mt-2' unoptimized/>
 
-      <p>Hey, this is my post</p>
+      <p className='mt-1'>Hey, this is my post</p>
 
-      <div className='flex justify-between items-center'>
+      <div className='flex justify-between items-center mt-4'>
         <div className='flex space-x-4'>
           <Heart/>
           <MessageCircle/>
           <Send/>
         </div>
         <Bookmark/>
+      </div>
+
+      <div className='flex space-x-4 p-2'>
+        <p>Likes 0</p>
+        <p>Replies 0</p>
       </div>
 
     </div>
