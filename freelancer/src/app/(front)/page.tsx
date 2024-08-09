8c75142bba53e0a7b708762f6300015c3e43db26
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-10 02:02:26 +0530

    Update - Added Supabase query to fetch posts and defined PostType and UserType in types.ts

--- freelancer/src/app/(front)/page.tsx
+++ freelancer/src/app/(front)/page.tsx
@@ -10,6 +10,11 @@ export default async function Home() {
 
   const {data, error} = await supabase.auth.getSession()
 
+  const {data:posts, error:PostErr} = await supabase.from("posts").select("id, content, image, likes_count, reply_count, created_at, users(id, name, username, profile_image)");
+
+  console.log("The posts are", posts);
+  console.log("The error is", PostErr);
+  
   return (
     <div>
       <PostCard/>
--- freelancer/types/types.ts
+++ freelancer/types/types.ts
@@ -3,3 +3,20 @@ type PostPayloadType = {
     image? : string;
     user_id : string;
 }
+
+type PostType = {
+    id: number,
+    content: string,
+    image?: string,
+    likes_count: number,
+    reply_count: number,
+    created_at: string,
+    users: any,
+}
+
+type UserType = {
+    id: number,
+    name: string,
+    username: string,
+    profile_image: string,
+}
