8e87dbd11584d06eb59c8f9140ede68265b23a0e
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-09 23:59:24 +0530

    Update - made the home page and the postCard component responsive for different size devices.

--- freelancer/src/app/(front)/layout.tsx
+++ freelancer/src/app/(front)/layout.tsx
@@ -11,11 +11,11 @@ export default async function FrontLayout({
     const supabase = createClient(cookies())
     const {data} = await supabase.auth.getSession()
     return (
-      <div className="container relative h-screen">
+      <div className="w-full p-2 md:container relative h-screen">
           <MobileAppNav/>
           <AppNav user={data.session?.user!}/>
           <div className="flex flex-col items-center h-full">
-            <div className="w-2/5">{children}</div>
+            <div className="w-full p-4 md:w-3/4 lg:w-2/5">{children}</div>
           </div>
       </div>
     );
