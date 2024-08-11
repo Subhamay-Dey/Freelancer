bd255cc1577ee14c47e918ee039db3506f71fba2
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-11 22:50:40 +0530

    sql_method.sql Updated - Added a new function `get_posts_with_likes` to retrieve posts with likes, and updated the file formatting.

--- freelancer/sql_methods.sql
+++ freelancer/sql_methods.sql
@@ -4,5 +4,50 @@ $$
   update posts
   set reply_count = reply_count + count
   where id = row_id
-$$ 
+$$
 language sql;
+
+-- Get posts with likes
+
+CREATE OR REPLACE FUNCTION get_posts_with_likes(request_user_id UUID)
+RETURNS TABLE (
+    post_id bigint,
+    user_id UUID,
+    content TEXT,
+    image TEXT,
+    name TEXT,
+    username varchar,
+    email TEXT,
+    profile_image text,
+    likes_count bigint,
+    reply_count bigint,
+    created_at timestamptz,
+    liked BOOLEAN
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        p.id as post_id,
+        p.user_id AS post_user_id,
+        p.content,
+        p.image,
+        u.name,
+        u.username,
+        u.email,
+        u.profile_image,
+        p.likes_count,
+        p.reply_count,
+        p.created_at,
+        EXISTS (
+            SELECT 1
+            FROM likes as l
+            WHERE l.post_id = p.id AND l.user_id::uuid = request_user_id
+        ) AS liked
+    FROM
+        posts as p
+    JOIN
+        users as u ON p.user_id = u.id;
+END;
+$$;
