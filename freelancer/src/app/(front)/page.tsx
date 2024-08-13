c1d8f013c0b9fffc5ee61489ce62de611d1bc398
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-13 13:43:58 +0530

    (front) page updated.

--- freelancer/src/app/(front)/page.tsx
+++ freelancer/src/app/(front)/page.tsx
@@ -6,16 +6,14 @@ import PostCard from "@/components/posts/PostCard";
 export default async function Home() {   
 
   const supabase = createClient(cookies())
-  const {data} = await supabase.auth.getSession()
+  const {data, error} = await supabase.auth.getSession()
 
-  const {data: posts, error} = await supabase.rpc("get_posts_with_likes", {request_user_id: data.session?.user.id})
-
-    console.log("The custom posts", posts);
-    console.log("The error is", error);
+  const {data: posts, error: customErr} = await supabase.rpc("get_posts_with_likes", {request_user_id: data.session?.user.id})
+  .order("post_id",{ascending: false})
 
   return (
     <div>
-      {posts && posts.length > 0 && posts.map((item: PostType, index: number) => (
+      {posts && posts.length > 0 && posts.map((item: PostType,index: number) => (
         <PostCard post={item} key={index}/>
       ))}
     </div>
