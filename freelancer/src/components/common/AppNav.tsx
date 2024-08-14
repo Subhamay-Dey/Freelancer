882e66b0d3a540423a4be88637d1a5d1be4caddf
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-14 12:33:35 +0530

    Update - updated AppNav component to include back button on non-static routes so added ArrowLeft icon to AppNav

--- freelancer/src/components/common/AppNav.tsx
+++ freelancer/src/components/common/AppNav.tsx
@@ -1,9 +1,9 @@
 "use client";
 import Image from 'next/image';
 import React from 'react';
-import {Bell, HomeIcon, Search, Settings, StickyNote, User} from "lucide-react";
+import { ArrowLeft, Bell, HomeIcon, Search, Settings, StickyNote, User} from "lucide-react";
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import AddPosts from '../posts/AddPosts';
 import { User as SupabaseUser } from '@supabase/supabase-js';
 import { SettingDropdown } from './Setting';
@@ -12,12 +12,18 @@ export default function AppNav({user}:{user:SupabaseUser}) {
   
   const pathname = usePathname();
   console.log("The current path is", pathname);
+
+  const router = useRouter();
+  const staticRoutes = ["/", "/search", "/notifications", "/profile"]
   
   return (
     <nav className='hidden md:flex justify-between items-center p-2'>
         <Image src={"/images/logo_512.png"} priority width={60} height={60} alt='logo'/>
 
-        <div className='flex space-x-12'>
+        <div className='flex space-x-12 items-center'>
+
+          {!staticRoutes.includes(pathname) && <ArrowLeft size={30} className='cursor-pointer bg-gray-600 hover:bg-gray-500 h-10 w-10 p-2 rounded-[50%]' onClick={() => router.back()}/>}
+
           <Link href={"/"} className={`cursor-pointer hover:text-foreground ${pathname === "/" ? "text-foreground" : "text-gray-500"} `}>
             <HomeIcon size={30}/>
           </Link>
@@ -44,4 +50,4 @@ export default function AppNav({user}:{user:SupabaseUser}) {
         <SettingDropdown/>
     </nav>
   )
-}
+} 
