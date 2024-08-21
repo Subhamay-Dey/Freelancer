7c9d85693756b0093a83904c1eaf80d5234321f1
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-21 23:29:46 +0530

    profile page updated and added some routes in protected route group.

--- freelancer/src/app/(front)/profile/page.tsx
+++ freelancer/src/app/(front)/profile/page.tsx
@@ -1,7 +1,14 @@
 import UserAvatar from '@/components/common/UserAvatar'
 import React from 'react'
 
-function Profile() {
+import {createClient} from "@/supabase/supabaseServer"
+import {cookies} from "next/headers"
+import { User } from '@supabase/supabase-js'
+
+async function Profile() {
+  const supabase = createClient(cookies())
+  const {data} = await supabase.auth.getSession()
+  const user:User = data.session?.user!
   return (
     <div className='flex justify-between items-center'>
         <div>
@@ -11,6 +18,6 @@ function Profile() {
         <UserAvatar name='Subhamay' width={5} height={5}/>
     </div>
   )
-}
+} 
 
 export default Profile
