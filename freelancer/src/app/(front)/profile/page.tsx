22e516dd949e4ce0c798e451e97ab943fafea8a1
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-21 23:57:37 +0530

    extracted user data from supabase user and rendered users details.

--- freelancer/src/app/(front)/profile/page.tsx
+++ freelancer/src/app/(front)/profile/page.tsx
@@ -4,18 +4,24 @@ import React from 'react'
 import {createClient} from "@/supabase/supabaseServer"
 import {cookies} from "next/headers"
 import { User } from '@supabase/supabase-js'
+import { Button } from '@/components/ui/button'
 
 async function Profile() {
   const supabase = createClient(cookies())
   const {data} = await supabase.auth.getSession()
   const user:User = data.session?.user!
   return (
-    <div className='flex justify-between items-center'>
-        <div>
-            <p className='text-2xl font-bold'>Subho</p>
-            <p className='font-bold'>@subho</p>
-        </div>
-        <UserAvatar name='Subhamay' width={5} height={5}/>
+    <div>
+      <div className='flex justify-between items-center'>
+          <div>
+              <p className='text-2xl font-bold'>{user.user_metadata?.["name"]}</p>
+              <p className='font-bold'>@{user.user_metadata?.["username"]}</p>
+          </div>
+          <UserAvatar name={user.user_metadata?.["name"]} width={5} height={5} image={user.user_metadata?.["profile_image"]}/>
+      </div>
+
+      <p className='mt-4'>{user.user_metadata?.["description"]}</p>
+      <Button variant={'outline'} className='w-full'>Edit Profile</Button>
     </div>
   )
 } 
