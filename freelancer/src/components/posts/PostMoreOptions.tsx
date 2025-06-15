849312196efae9de9a76a0afeb6012b35db3f4cf
Subhamay-Dey <ncdey1966@gmail.com>
2025-06-16 00:15:01 +0530

    User Post Deletion completed ✅

--- freelancer/src/components/posts/PostMoreOptions.tsx
+++ freelancer/src/components/posts/PostMoreOptions.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import {
   DropdownMenu,
@@ -6,18 +8,58 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { MoreVertical } from 'lucide-react'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from '../ui/button'
+import { createClient } from '@/supabase/supabaseClient'
 
 function PostMoreOptions({userId, post}: {userId: string, post: PostType}) {
 
+    const [open, setOpen] = React.useState(false);
+    const supabase = createClient()
+
+    const deletePost = async() => {
+        await supabase.from("posts").delete().eq('id', post.post_id)
+        setOpen(false);
+        window.location.reload();
+    }
+
   return (
     <div>
+        <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="border-none">
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              post and remove it's data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="destructive" onClick={deletePost}>
+              Yes Delete it!
+            </Button>
+            <DialogClose asChild>
+              <Button variant="secondary">Cancel</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <MoreVertical className='cursor-pointer' />
             </DropdownMenuTrigger>
             <DropdownMenuContent>
                 <DropdownMenuItem>Info</DropdownMenuItem>
-                {userId === post.user_id && (<DropdownMenuItem>Delete</DropdownMenuItem>)}
+                {userId === post.user_id && (<DropdownMenuItem onClick={() => setOpen(true)}>Delete</DropdownMenuItem>)}
                 
             </DropdownMenuContent>
         </DropdownMenu>
